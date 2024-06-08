09b48915be0e179a41eb47259a5af6462d03d50f
Niki Khatun <niki.sine90@gmail.com>
2024-06-08 21:18:14 +0530

    added all matlab assignment codes

---
+++ MATLAB_DRIVE/Q4.m
@@ -0,0 +1,6 @@
+img = imread('cameraman.tif');
+threshold = 100; % Example threshold
+binary_img = img > threshold;
+figure,
+subplot(1,2,1), imshow(img), title('Original Image')
+subplot(1,2,2), imshow(binary_img), title(['Binary Image with Threshold ', num2str(threshold)])
